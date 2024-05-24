dad111a26f75543ce2c541eb2f63d84946ec1acb
Noa Second <contact@noasecond.com>
2024-05-24 16:23:31 +0200

    🌐 Add translation file

---
+++ GUI.ts
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="GUI.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="43"/>
+        <source>Injection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="64"/>
+        <source>Aimant</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="109"/>
+        <source>Courant bobine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="135"/>
+        <location filename="GUI.ui" line="171"/>
+        <location filename="GUI.ui" line="277"/>
+        <location filename="GUI.ui" line="572"/>
+        <location filename="GUI.ui" line="624"/>
+        <location filename="GUI.ui" line="686"/>
+        <location filename="GUI.ui" line="722"/>
+        <location filename="GUI.ui" line="758"/>
+        <location filename="GUI.ui" line="809"/>
+        <location filename="GUI.ui" line="845"/>
+        <location filename="GUI.ui" line="884"/>
+        <source>µA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="158"/>
+        <source>Courant effectif</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="199"/>
+        <source>Bobine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="206"/>
+        <location filename="GUI.ui" line="426"/>
+        <source>RF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="251"/>
+        <source>Amplificateur</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="300"/>
+        <source>Tension crête</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="326"/>
+        <source>KV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="343"/>
+        <source>Position effective piston d&apos;accord</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="378"/>
+        <source>Position piston d&apos;accord</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="433"/>
+        <source>Extraction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="469"/>
+        <source>Cible 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="482"/>
+        <source>Cible 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="495"/>
+        <location filename="GUI.ui" line="638"/>
+        <source>Stripper 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="508"/>
+        <location filename="GUI.ui" line="651"/>
+        <source>Stripper 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="521"/>
+        <location filename="GUI.ui" line="664"/>
+        <source>Stripper 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="543"/>
+        <source>Rayon canne cible 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="595"/>
+        <source>Rayon canne cible 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="786"/>
+        <source>Cibles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="904"/>
+        <source>Vide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="941"/>
+        <source>Vide primaire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="980"/>
+        <source>Vide secondaire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="987"/>
+        <source>Eau</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="995"/>
+        <source>Paramètres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1031"/>
+        <source>Chemin du dossier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1053"/>
+        <source>Fréquence d&apos;enregistrement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1082"/>
+        <source>Sec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1105"/>
+        <source>Limite de taille par fichiers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1134"/>
+        <source>Mb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1148"/>
+        <source>Activer l&apos;enregistrement des données</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1161"/>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1202"/>
+        <source>Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1219"/>
+        <source>Enregistrement des données</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1257"/>
+        <source>Ports disponibles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1270"/>
+        <source>Rafraichir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI.ui" line="1303"/>
+        <source>Ports série</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
